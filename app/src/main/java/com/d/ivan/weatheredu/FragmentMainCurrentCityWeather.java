c1e3b7a49f5227ddfde5a404e6d509fdb1023ba2
div-3 <div-3@mail.ru>
2018-04-09 23:53:01 +0800

    .

--- app/src/main/java/com/d/ivan/weatheredu/FragmentMainCurrentCityWeather.java
+++ app/src/main/java/com/d/ivan/weatheredu/FragmentMainCurrentCityWeather.java
@@ -1,5 +1,6 @@
 package com.d.ivan.weatheredu;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,6 +16,8 @@ public class FragmentMainCurrentCityWeather extends Fragment {
         public void onCityWeatherLoadError(String currentCity);
     }
 
+    OnCurrentCityChangeListener mCallback;  //Создание экземпляра интерфейса для передачи данных в callback'е активности
+
 
     //Привязка к разметке
     @Nullable
@@ -22,5 +25,28 @@ public class FragmentMainCurrentCityWeather extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main_current_city_weather, container, false);
     }
-    
+
+
+    //Проверка, что активность переопределила метод из интерфейса фрагмента
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            mCallback = (OnCurrentCityChangeListener) context;
+        } catch (ClassCastException cce){
+            throw new ClassCastException(context.toString() + " must implement OnCurrentCityChangeListener");
+        }
+    }
+
+    //Callback для активности, если что-то произошло при загрузке данных
+    public void smthWrongHappend(String currentCity){
+        mCallback.onCityWeatherLoadError(currentCity);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Получение данных от погодного сервера https://openweathermap.org/api
+    ///////////////////////////////////////////////////////////////////////////
+
+
 }
